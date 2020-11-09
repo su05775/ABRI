508ba60762c5f060036fd140f72228614c2caaa5
su05775 <su05775@naver.com>
2020-11-09 11:25:45 +0900

    2020.11.09

--- js/js.js
+++ js/js.js
@@ -6,3 +6,15 @@ $(function(){
         $('.popup').fadeOut();
     });
 });
+// 팝업창
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  
+// 스와이퍼
